ccf7006fefd9b3c6d016b31ea4b5490c4e5cd284
Mateo Monroy <monroymateo1@gmail.com>
2025-02-02 17:48:09 -0500

    j

---
+++ script.js
@@ -0,0 +1,36 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Marcela Muñoz Muñoz
+TEL: +57 3167597517
+NOTE: Directora Comercial
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function descargarContacto1() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Jimenez Puerta Abogados
+TEL: +57 3187324455
+NOTE: facturacion@jimenezpuerta.com
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
